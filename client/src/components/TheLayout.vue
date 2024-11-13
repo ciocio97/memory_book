25c2d4b57a90acad1698f2386ba78887e5114575
lsy <ciociolee97@gmail.com>
2024-11-13 15:31:04 +0900

    feat: add vue-router and create layout

---
+++ client/src/components/TheLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="layout">
+        <div class="container">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheLayout",
+}
+</script>
+
+<style>
+.layout {
+    /* remove scroll width */
+    width: calc(100vw - 2px);
+    height: calc(100vh - 2px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid;
+
+    .container {
+        max-width: 320px;
+        max-height: calc(100vh - 100px);
+        overflow: hidden;
+        border: 1px solid;
+    }
+}
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,17 @@
+import TheHome from '@/views/TheHome.vue';
+import TheReader from '@/views/TheReader.vue';
+import TheWriter from '@/views/TheWriter.vue';
+import { createMemoryHistory, createRouter } from 'vue-router';
+
+const routes = [
+  { path: '/', component: TheHome },
+  { path: '/write', component: TheWriter },
+  { path: '/read', component: TheReader }
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+export default router;
