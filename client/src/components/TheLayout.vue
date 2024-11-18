366a092e13d4556196b6c57ba0723082352ae118
lsy <ciociolee97@gmail.com>
2024-11-18 16:00:19 +0900

    feat: complete stars orbit background

--- client/src/components/TheLayout.vue
+++ client/src/components/TheLayout.vue
@@ -9,133 +9,90 @@
 
 <script setup>
 import { onMounted, ref, useTemplateRef } from 'vue';
+import { debounce } from '@/utils/common';
+import { createStar } from '@/objects/star';
 
 const canvasRef = useTemplateRef('canvas');
 const canvasContext = ref(null);
+let starCanvas, starContext, gradient;
+let stars = [];
 
-const width = ref(0);
-const height = ref(0);
-const maxCount = 1400;
-const stars = [];
-
-const starCanvas = document.createElement('canvas');
-const starContext = starCanvas.getContext('2d');
-starContext.width = 100;
-starContext.height = 100;
-
-class Star {
-  constructor(width, height) {
-    this.orbitRadius = random(maxOrbit(width, height));
-    this.radius = random(60, this.orbitRadius) / 12;
-    this.orbitX = width / 2;
-    this.orbitY = height / 2;
-    this.timePassed = random(0, maxCount);
-    this.speed = random(this.orbitRadius) / 50000;
-    this.alpha = random(2, 10) / 10; // 0.5 ~ 1.1
-  }
-
-  draw(ctx) {
-    const x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX;
-    const y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY;
-    const twinkle = random(10); // 0 ~ 10
-
-    // if (twinkle === 1 && this.alpha > 0) {
-    //   this.alpha -= 0.05;
-    // } else if (twinkle === 2 && this.alpha < 1) {
-    //   this.alpha += 0.05;
-    // }
-
-    const dx = x - this.radius / 2;
-    const dy = y - this.radius / 2;
-    const dWidth = this.radius;
-    const dHeight = this.radius;
-
-    ctx.globalAlpha = this.alpha;
-    ctx.drawImage(starCanvas, dx, dy, dWidth, dHeight);
-    this.timePassed += this.speed;
-  }
-}
-
-const setBackground = () => {
-  // create stars
-  for (let i = 0; i < maxCount; i++) {
-    stars.push(new Star(width.value, height.value));
-  }
-
-  animation();
-};
-
-const random = (min, max) => {
-  let minVal = min;
-  let maxVal = max;
-
-  // min: 0, max: argument
-  if (max === undefined) {
-    maxVal = min;
-    minVal = 0;
-  }
-
-  // max min align
-  if (min > max) {
-    const tmp = max;
-    maxVal = min;
-    minVal = tmp;
-  }
-
-  // random number + min
-  return Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal;
-};
-
-const maxOrbit = (x, y) => {
-  const max = Math.max(x, y);
-  // a^2 + b^2 = c^2
-  const diameter = Math.round(Math.sqrt(max * max + max * max));
-
-  return diameter / 2;
-};
-
-const animation = () => {
-  const ctx = canvasContext.value;
-
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.globalAlpha = 0.8;
-  ctx.fillStyle = 'hsla(' + 217 + ', 64%, 6%, 1)';
-  ctx.fillRect(0, 0, width.value, height.value);
-
-  ctx.globalCompositeOperation = 'lighter';
-
-  for (let star of stars) {
-    star.draw(ctx);
-  }
-
-  window.requestAnimationFrame(animation);
-};
+const width = ref(window.innerWidth);
+const height = ref(window.innerHeight);
+const maxStars = 1400;
 
 onMounted(() => {
+  initConst();
+  initCanvas();
+  initStars();
+  animation();
+});
+
+const onResizeHandler = () => {
   width.value = window.innerWidth;
   height.value = window.innerHeight;
 
+  initCanvas();
+  initStars();
+  animation();
+};
+
+window.addEventListener('resize', debounce(onResizeHandler, 1000));
+
+const initConst = () => {
+  starCanvas = document.createElement('canvas');
+  starContext = starCanvas.getContext('2d');
+  starCanvas.width = 100;
+  starCanvas.height = 100;
+
+  const half = starCanvas.width / 2;
+  gradient = starContext.createRadialGradient(half, half, 0, half, half, half);
+  gradient.addColorStop(0.025, '#fff');
+  gradient.addColorStop(0.1, `hsl(217, 61%, 33%)`);
+  gradient.addColorStop(0.25, `hsl(217, 64%, 6%)`);
+  gradient.addColorStop(1, 'transparent');
+
+  starContext.fillStyle = gradient;
+  starContext.beginPath();
+  starContext.arc(half, half, half, 0, Math.PI * 2);
+  starContext.fill();
+};
+
+const initCanvas = () => {
   const canvas = canvasRef.value;
   canvas.width = width.value;
   canvas.height = height.value;
   canvasContext.value = canvas.getContext('2d');
+};
 
-  setBackground();
+const initStars = () => {
+  stars = Array(maxStars)
+    .fill(0)
+    .map(() =>
+      createStar(
+        width.value,
+        height.value,
+        maxStars,
+        canvasContext,
+        starCanvas,
+      ),
+    );
+};
 
-  const half = starCanvas.width / 2;
-  const gradient = starContext.createRadialGradient(
-    half,
-    half,
-    0,
-    half,
-    half,
-    half,
-  );
-  gradient.addColorStop(0.025, '#fff');
-  gradient.addColorStop(0.1, 'hsl(' + 217 + ', 61%, 33%)');
-  gradient.addColorStop(0.25, 'hsl(' + 217 + ', 64%, 6%)');
-  gradient.addColorStop(1, 'transparent');
-});
+const animation = () => {
+  canvasContext.value.globalCompositeOperation = 'source-over';
+  canvasContext.value.globalAlpha = 0.8;
+  canvasContext.value.fillStyle = `hsla(217, 64%, 6%, 1)`;
+  canvasContext.value.fillRect(0, 0, width.value, height.value);
+
+  canvasContext.value.globalCompositeOperation = 'lighter';
+
+  for (const star of stars) {
+    star.draw();
+  }
+
+  requestAnimationFrame(animation);
+};
 </script>
 
 <style>
