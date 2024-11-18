606f7f34367780b4fdc9055ac58a8bc7b86d4340
lsy <ciociolee97@gmail.com>
2024-11-18 16:44:29 +0900

    feat: add main logo text

--- client/src/views/TheHome.vue
+++ client/src/views/TheHome.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>나에게 메모를 남겨줘</div>
+  <div class="logoContainer">
+    <img
+      class="mainLogo"
+      src="../assets/main_logo_text.svg"
+      alt="please_leave_me_a_message"
+    />
+  </div>
   <button class="loginButtons" @click.once="onClickKakaoLoginButton">
-    <img class="kakaoImage" src="../assets/kakao_login_image.png" />
+    <img
+      class="kakaoImage"
+      src="../assets/kakao_login_image.png"
+      alt="kakao_login_btn"
+    />
   </button>
 </template>
 
@@ -12,6 +22,13 @@ const onClickKakaoLoginButton = () => {
 </script>
 
 <style>
+.logoContainer {
+  margin-bottom: 40px;
+  .mainLogo {
+    width: 15rem;
+  }
+}
+
 .loginButtons {
   width: 100%;
   border: none;
