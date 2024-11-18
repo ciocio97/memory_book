3019d35072c074fcd7ba5597ebb0cc63ad6a1191
lsy <ciociolee97@gmail.com>
2024-11-18 18:02:36 +0900

    feat: add sticker images

--- client/src/views/TheWriter.vue
+++ client/src/views/TheWriter.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>쓰기 페이지</div>
+  <div class="container">쓰기 페이지</div>
+  <div v-for="(image, index) in images">
+    <button>
+      <img :key="'sticker' + index" :src="image.src" :alt="'sticker' + index" />
+    </button>
+  </div>
+  <!-- <img src="../assets/images/sticker/sticker_rainbow.svg" /> -->
 </template>
 
-<script>
-export default {
-  name: 'TheWriter',
-};
+<script setup>
+const images = [
+  { src: '@/assets/sticker_rainbow.svg' },
+  { src: '@/assets/sticker_butterfly.svg' },
+  { src: '@/assets/sticker_sun.svg' },
+  { src: '@/assets/sticker_star.svg' },
+  { src: '@/assets/sticker_cloud.svg' },
+  { src: '@/assets/sticker_earth.svg' },
+];
 </script>
 
-<style></style>
+<style>
+.container {
+  background-color: white;
+  border-radius: 0.5rem;
+}
+</style>
