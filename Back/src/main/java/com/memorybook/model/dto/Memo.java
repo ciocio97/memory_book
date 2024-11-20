a09b01c2fae7cc059ffdeb0d63234b0f43f77191
gayoung <ppoppo1217@naver.com>
2024-11-20 11:23:55 +0900

    folder name change

---
+++ Back/src/main/java/com/memorybook/model/dto/Memo.java
@@ -0,0 +1,60 @@
+package com.memorybook.model.dto;
+
+public class Memo {
+	private int memoId;
+	private String text;
+	private int imgNum;
+	private String writer;
+	private String reader;
+	
+	public Memo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Memo(String text, int imgNum, String writer, String reader) {
+		this.text = text;
+		this.imgNum = imgNum;
+		this.writer = writer;
+		this.reader = reader;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getImgNum() {
+		return imgNum;
+	}
+
+	public void setImgNum(int imgNum) {
+		this.imgNum = imgNum;
+	}
+
+	public String getReader() {
+		return reader;
+	}
+
+	public void setReader(String reader) {
+		this.reader = reader;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public int getMemoId() {
+		return memoId;
+	}
+
+	public void setMemoId(int memoId) {
+		this.memoId = memoId;
+	}	
+}
--- MemoryBook/Back/src/main/java/com/memorybook/model/service/RefreshTokenService.java
+++
@@ -1,9 +0,0 @@
-package com.memorybook.model.service;
-
-public interface RefreshTokenService {
-
-	String createAndSaveRefreshToken(String userId);
-
-	String getUserIdByToken(String refreshToken);
-
-}
